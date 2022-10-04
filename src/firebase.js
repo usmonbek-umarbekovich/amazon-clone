fc0441b6fcb3571a4a2afd27911fc995f929882b
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-05 00:16:06 +0200

    added firebase basic config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBdG5szZoagTDYEyhbzVzxJbx4Je9LcN40",
+  authDomain: "clone-1764d.firebaseapp.com",
+  projectId: "clone-1764d",
+  storageBucket: "clone-1764d.appspot.com",
+  messagingSenderId: "276015548783",
+  appId: "1:276015548783:web:39ebf0573e54ca0004c09a",
+  measurementId: "G-QB5J0F3XPC"
+};
