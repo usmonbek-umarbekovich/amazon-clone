6e16961d31a5b7b0790db185ba98e99d257d5637
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-05 14:02:36 +0200

    added Home component & stylesheet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './Header';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home/>
     </div>
   );
 }
