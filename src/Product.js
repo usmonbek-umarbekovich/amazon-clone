873a28a13225b443e690bec255071788c23aeaec
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-05 18:32:12 +0200

    fixed bug

--- src/Product.js
+++ src/Product.js
@@ -27,8 +27,8 @@ function Product({ id, title, image, price, rating }) {
         <div className="product__rating">
           {Array(rating)
             .fill()
-            .map(_ => (
-              <p>⭐</p>
+            .map((_, i) => (
+              <p key={i}>⭐</p>
             ))}
         </div>
       </div>
