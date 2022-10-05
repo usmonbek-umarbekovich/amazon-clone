f3ebc006f5d07a2a391267e584a03e9c73836e46
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-05 18:31:14 +0200

    added CheckoutProduct & stylesheet

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,35 @@
+import { useStateValue } from './StateProvider';
+import './CheckoutProduct.css';
+
+function CheckoutProduct({ id, index, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    dispatch({
+      type: 'REMOVE_FROM_BASKET',
+      payload: { id, index },
+    });
+  };
+
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image" src={image} alt={title} />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p key={i}>⭐</p>
+            ))}
+        </div>
+        <button onClick={removeFromBasket}>Remove from basket</button>
+      </div>
+    </div>
+  );
+}
+export default CheckoutProduct;
