344ae26b86e4b84812a6e33a27c2e9f5cbb18d9c
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-05 17:38:51 +0200

    added context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './reducer';
 import App from './App';
 import './index.css';
 
@@ -8,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
