9c7a24018c36e19534252ec7ec3c4fb3277004ba
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-10 00:47:37 +0200

    showing email on checkout

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -4,7 +4,7 @@ import CheckoutProduct from '../../components/CheckoutProduct';
 import './style.css';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -15,6 +15,7 @@ function Checkout() {
           alt="Banner"
         />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
           {basket.map((product, index) => (
             <CheckoutProduct
