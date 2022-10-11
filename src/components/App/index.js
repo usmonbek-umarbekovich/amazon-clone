94f030307ee18e49bd7290c0f1a47d9d42eaae25
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-12 00:46:55 +0200

    added stripe & Payment

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,20 @@
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import { auth } from '../../config';
 import { useStateValue } from '../../contexts/StateProvider';
 import Header from '../../layouts/Header';
 import Home from '../../pages/Home';
 import Login from '../../pages/Login';
 import Checkout from '../../pages/Checkout';
+import Payment from '../../pages/Payment';
 import './style.css';
+
+const promise = loadStripe(
+  'pk_test_51LrmN4CMUJYb034JpqEEBFp1laHuSskqorlDHr1hAudyNGghY7nmzZMRbou9rQA1flPGNi0CzV4xuONFVvQ9sT2W00svwZtrei'
+);
 
 function App() {
   const [_, dispatch] = useStateValue();
@@ -37,6 +44,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="checkout" element={<Checkout />} />
+        <Route
+          path="payment"
+          element={
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          }
+        />
       </Routes>
     </div>
   );
