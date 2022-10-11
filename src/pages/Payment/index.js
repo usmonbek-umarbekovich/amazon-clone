4a61ff9528b785a2ecf526e9ecdf51e982ecd1ce
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-12 00:46:27 +0200

    created Payment component & its stylesheet

---
+++ src/pages/Payment/index.js
@@ -0,0 +1,127 @@
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format';
+import CheckoutProduct from '../../components/CheckoutProduct';
+import { useStateValue } from '../../contexts/StateProvider';
+import { getBasketTotal } from '../../services/reducer';
+import axios from '../../services/axios';
+import './style.css';
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const navigate = useNavigate();
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [processing, setProcessing] = useState(false);
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: 'post',
+        url: `/payments/create/total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [basket]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    setProcessing(true);
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        setSucceeded(true);
+        setError('');
+        navigate('/orders', { replace: true });
+      })
+      .finally(() => setProcessing(false));
+  };
+
+  const handleChange = e => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : '');
+  };
+
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (
+          <Link to="/checkout">
+            {basket?.length ?? 0} item{basket?.length ? 's' : ''}
+          </Link>
+          )
+        </h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map(p => (
+              <CheckoutProduct
+                key={p.id}
+                id={p.id}
+                title={p.title}
+                image={p.image}
+                price={p.price}
+                rating={p.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={value => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType="text"
+                  thousandSeperator={true}
+                  prefix="$"
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? 'Processing' : 'Buy Now'}</span>
+                </button>
+              </div>
+
+              {error && <div>{error}</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Payment;
