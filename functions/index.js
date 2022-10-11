e3c9ddc81450858b9436f3e973d7962216340d47
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-12 01:01:07 +0200

    added envs

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')(
-  'sk_test_51LrmN4CMUJYb034JbjVq0I6EHWYpAHDTNY064z9KDHfLpMnAfKEZP0i8iVycyoGl2QqthlcDknBkYQ6JlYcaVgdt00zWqOOmOi'
-);
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
+
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const app = express();
 
