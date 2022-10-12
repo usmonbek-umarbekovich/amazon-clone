2d368d0dc6c24acd33e28ee5176c325eb2c38d33
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-12 22:14:55 +0200

    added payment route

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const app = express();
@@ -12,6 +11,17 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).send('hello world'));
+app.get('/', (req, res) => res.status(200).send('Hello World'));
+app.post('/payments/create', async (req, res) => {
+  const { total } = req.query;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: 'usd',
+  });
+
+  res.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 exports.api = functions.https.onRequest(app);
