1e9c8be7650afa0fbb52d5c227d17da54301988c
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-12 22:16:03 +0200

    added localhost url for testing

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '...',
+  baseURL: 'http://localhost:5001/clone-1764d/us-central1/api',
+});
 
-})
-
-export default instance
+export default instance;
