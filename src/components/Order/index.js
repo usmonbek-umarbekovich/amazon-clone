7385b84d9cc38e4c03bade9a2c59e56fdb64fa6c
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-13 22:35:30 +0200

    added Order component and styled it

---
+++ src/components/Order/index.js
@@ -0,0 +1,36 @@
+import moment from 'moment';
+import CurrencyFormat from 'react-currency-format';
+import CheckoutProduct from '../../components/CheckoutProduct';
+import './style.css';
+
+function Order({ order }) {
+  return (
+    <div className="order">
+      <h2>Order</h2>
+      <p>{moment.unix(order.data.created).format('MMMM Do YYYY, h:mma')}</p>
+      <p className="order__id">
+        <small>{order.id}</small>
+      </p>
+      {order.data.basket?.map(p => (
+        <CheckoutProduct
+          key={p.id}
+          id={p.id}
+          title={p.title}
+          image={p.image}
+          price={p.price}
+          rating={p.rating}
+          hideButton
+        />
+      ))}
+      <CurrencyFormat
+        renderText={value => <h3>Order Total: {value}</h3>}
+        decimalScale={2}
+        value={order.data.amount / 100}
+        displayType="text"
+        thousandSeperator={true}
+        prefix="$"
+      />
+    </div>
+  );
+}
+export default Order;
