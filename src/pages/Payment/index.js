8fd43a95e4832ed46633722a263ad307f50a1801
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-13 02:26:44 +0200

    handled payment info

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { doc, setDoc } from 'firebase/firestore';
 import CurrencyFormat from 'react-currency-format';
 import CheckoutProduct from '../../components/CheckoutProduct';
 import { useStateValue } from '../../contexts/StateProvider';
 import { getBasketTotal } from '../../services/reducer';
 import axios from '../../services/axios';
+import { db } from '../../config';
 import './style.css';
 
 function Payment() {
@@ -38,18 +40,30 @@ function Payment() {
     e.preventDefault();
 
     setProcessing(true);
-    const payload = await stripe
-      .confirmCardPayment(clientSecret, {
+    try {
+      const { paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
         payment_method: {
           card: elements.getElement(CardElement),
         },
-      })
-      .then(({ paymentIntent }) => {
-        setSucceeded(true);
-        setError('');
-        navigate('/orders', { replace: true });
-      })
-      .finally(() => setProcessing(false));
+      });
+
+      if (user) {
+        const orderRef = doc(db, 'users', user.uid, 'orders', paymentIntent.id);
+        await setDoc(orderRef, {
+          basket,
+          amount: paymentIntent.amount,
+          created: paymentIntent.created,
+        });
+      }
+
+      setSucceeded(true);
+      setError('');
+      dispatch({ type: 'EMPTY_BASKET' });
+      navigate('/orders', { replace: true });
+    } catch (err) {
+      console.log(err);
+    }
+    setProcessing(false);
   };
 
   const handleChange = e => {
