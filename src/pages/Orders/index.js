44aebf093286b2b66908ee276b8f79ebc72d8af1
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-13 22:38:02 +0200

    showed user's orders and styled them

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -2,28 +2,36 @@ import { useEffect, useState } from 'react';
 import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 import { useStateValue } from '../../contexts/StateProvider';
 import { db } from '../../config';
+import Order from '../../components/Order';
 import './style.css';
 
 function Orders() {
-  const [{ basket, user }, dispatch] = useStateValue();
+  const [{ user }] = useStateValue();
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    if (!user) return;
+    if (!user) return setOrders([]);
 
     const ordersRef = collection(db, 'users', user.uid, 'orders');
     const q = query(ordersRef, orderBy('created', 'desc'));
     onSnapshot(q, snapshot => {
-      setOrders(snapshot.docs.map(doc => ({
-        id: doc.id,
-        data: doc.data()
-      })))
-    })
-  }, []);
+      setOrders(
+        snapshot.docs.map(doc => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, [user]);
 
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map(order => (
+          <Order key={order.id} order={order} />
+        ))}
+      </div>
     </div>
   );
 }
