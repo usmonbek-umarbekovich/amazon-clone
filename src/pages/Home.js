e498d23524bd26366484b4bad0fdcdd7ba88b559
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-10-30 17:45:51 +0100

    fixed responsiveness on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
+import { useRef, useState, useEffect } from 'react';
 import ProductList from '../components/ProductList';
 import './style.css';
 
 function Home() {
+  const [bannerMargin, setBannerMargin] = useState('-10rem');
+  const bannerRef = useRef();
+
+  useEffect(() => {
+    if (!bannerRef.current) return;
+    setBannerMargin(
+      `-${bannerRef.current.getBoundingClientRect().height / 2}px`
+    );
+
+    const controller = new AbortController();
+    window.addEventListener(
+      'resize',
+      () =>
+        setBannerMargin(
+          `-${bannerRef.current.getBoundingClientRect().height / 2}px`
+        ),
+      { signal: controller.signal }
+    );
+
+    return () => controller.abort();
+  }, []);
+
   return (
     <div id="home">
       <img
-        className="home__image"
+        ref={bannerRef}
+        style={{ marginBottom: bannerMargin }}
+        className="home__image w-100"
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_jpg"
         alt="Banner"
       />
