0e63a9dcfa14e0884a313cf81b8b63e16c123d4c
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-11-28 19:11:06 +0000

    fixed bugs & size issues

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -51,7 +51,9 @@ function Orders() {
           </Breadcrumb>
         </header>
 
-        <section id="orders-search-section" className="mb-sm-2 order-1 order-sm-0">
+        <section
+          id="orders-search-section"
+          className="mb-sm-2 order-1 order-sm-0">
           <div className="d-flex align-items-center justify-content-between">
             <h1 className="d-none d-sm-block fs-3">My orders</h1>
             <Form className="d-flex col-md-6 col-sm-8 col-12">
@@ -63,23 +65,20 @@ function Orders() {
                   size="sm"
                   type="search"
                   name="orderName"
-                  id="orderName"
                   placeholder="All orders"
                   aria-label="Order name"
-                  style={{ paddingLeft: '1.75rem' }}
                 />
-                <FaSearch
-                  style={{ left: '0.5rem', fontSize: '0.875rem' }}
-                  className="position-absolute top-50 translate-middle-y"
-                />
+                <FaSearch className="position-absolute top-50 translate-middle-y" />
                 <Button
                   variant="link"
                   style={{ right: '0.5rem', fontSize: '0.875rem' }}
-                  className="d-sm-none position-absolute top-50 translate-middle-y p-0 ps-2 text-reset text-decoration-none border-start">
-                  <span style={{ fontSize: '0.875rem' }} className="me-2">
+                  className="d-sm-none position-absolute top-50 translate-middle-y p-2 pe-0 text-reset text-decoration-none border-start bg-white">
+                  <span
+                    style={{ fontSize: '0.9375rem' }}
+                    className="me-2 fw-semibold">
                     Filter
                   </span>
-                  <FaChevronRight style={{ fontSize: '0.875rem' }} />
+                  <FaChevronRight style={{ fontSize: '0.9375rem' }} />
                 </Button>
               </Form.Group>
               <Button
@@ -102,29 +101,51 @@ function Orders() {
             onSelect={eventKey => setActiveNavKey(eventKey)}>
             <Nav.Item as="li" className="me-3">
               {activeNavKey === 'orders' ? (
-                <h3 className="fw-bold border-bottom border-2 border-warning p-2 mb-0">
-                  Orders
-                </h3>
+                <>
+                  <h3 className="d-none d-sm-block fw-bold border-bottom border-3 border-warning p-2">
+                    Orders
+                  </h3>
+                  <Nav.Link
+                    eventKey="orders"
+                    className="d-sm-none fw-bold border-bottom border-3 border-primary p-2">
+                    My orders
+                  </Nav.Link>
+                </>
               ) : (
                 <Nav.Link eventKey="orders" className="link-success p-2">
-                  Orders
+                  <span className="d-none d-sm-inline link-success">
+                    Orders
+                  </span>
+                  <span className="d-sm-none link-dark">My orders</span>
                 </Nav.Link>
               )}
             </Nav.Item>
+
             <Nav.Item as="li" className="me-3">
               {activeNavKey === 'buy-again' ? (
-                <h3 className="fw-bold border-bottom border-2 border-warning p-2 mb-0">
-                  Buy again
-                </h3>
+                <>
+                  <h3 className="d-none d-sm-block fw-bold border-bottom border-3 border-warning p-2">
+                    Buy again
+                  </h3>
+                  <Nav.Link
+                    eventKey="buy-again"
+                    className="d-sm-none fw-bold border-bottom border-3 border-primary p-2">
+                    Buy again
+                  </Nav.Link>
+                </>
               ) : (
-                <Nav.Link eventKey="buy-again" className="link-success p-2">
-                  Buy again
+                <Nav.Link eventKey="buy-again" className="p-2">
+                  <span className="d-none d-sm-inline link-success">
+                    Buy again
+                  </span>
+                  <span className="d-sm-none link-dark">Buy again</span>
                 </Nav.Link>
               )}
             </Nav.Item>
+
             <Nav.Item as="li" className="me-3 d-none d-sm-block">
               {activeNavKey === 'not-shipped-yet' ? (
-                <h3 className="fw-bold border-bottom border-2 border-warning p-2 mb-0">
+                <h3 className="fw-bold border-bottom border-3 border-warning p-2">
                   Not shipped yet
                 </h3>
               ) : (
@@ -135,9 +156,10 @@ function Orders() {
                 </Nav.Link>
               )}
             </Nav.Item>
+
             <Nav.Item as="li" className="me-3 d-none d-sm-block">
               {activeNavKey === 'canceled-orders' ? (
-                <h3 className="fw-bold border-bottom border-2 border-warning p-2 mb-0">
+                <h3 className="fw-bold border-bottom border-3 border-warning p-2">
                   Canceled orders
                 </h3>
               ) : (
@@ -153,7 +175,11 @@ function Orders() {
 
         <section id="orders-date-section" className="mb-sm-3 order-2">
           <div className="d-sm-none">
-            <p className="text-secondary text-center fw-semibold mb-0">Last three months</p>
+            <p
+              style={{ fontSize: '0.8125rem' }}
+              className="text-secondary text-center fw-bold my-1">
+              Last three months
+            </p>
           </div>
           <div className="d-none d-sm-flex align-items-center">
             <p className="me-2 mb-0">
